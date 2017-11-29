f384eec9abcacd2c4956768dfe768e5636263be9
schoeppeandreas <schoeppeandreas@gmail.com>
2017-11-29 23:05:20 +0100

    Bug fixes, error when list is empty

--- src/frames/ProzessAllocated.java
+++ src/frames/ProzessAllocated.java
@@ -11,14 +11,23 @@ public class ProzessAllocated {
 	private static DefaultTableModel model = new DefaultTableModel();
 	
 	private static void printSelectedAllocatedItems() {
-		if(!selectedAllocatedItems.isEmpty())
+		System.out.println(selectedAllocatedItems.size());
+		if(selectedAllocatedItems.size() != 0) {
 			Tsp400Label_Allocated.output(selectedAllocatedItems);
+		}
 	}
 	
-	public static DefaultTableModel printAllParts() {
+	private static void setDefault() {
 		model = new DefaultTableModel();
 		allocatedItems = new ArrayList<AllocatedItem>();
 		allocatedItems = ReadAllocatedFromXls.read();
+		if(selectedAllocatedItems.size() != 0) {
+			selectedAllocatedItems.clear();
+		}
+	}
+	
+	public static DefaultTableModel printAllParts() {
+		setDefault();
 		for (int i = 0; i < allocatedItems.size(); i++) {
 			AllocatedItem allocatedItem = allocatedItems.get(i);
 			if (i == 0) { // Ueberschrift ableiten
@@ -36,9 +45,7 @@ public class ProzessAllocated {
 	}
 	
 	public static DefaultTableModel printReturnN_Parts () {
-		model = new DefaultTableModel();
-		allocatedItems = new ArrayList<AllocatedItem>();
-		allocatedItems = ReadAllocatedFromXls.read();
+		setDefault();
 		for (int i = 0; i < allocatedItems.size(); i++) {
 			AllocatedItem allocatedItem = allocatedItems.get(i);
 			if (i == 0) { // Ueberschrift ableiten
@@ -58,42 +65,10 @@ public class ProzessAllocated {
 	}
 	
 	public static DefaultTableModel printByPnTextSearch(String newPN) {
-		model = new DefaultTableModel();
-		//table.setModel(model);
-		
-		// model.addColumn("APPLICANT");
-		// model.addColumn("IS_KEY_PART");
-		// model.addColumn("LOCATION");
-		// model.addColumn("NEW_PART_DESC");
-		// model.addColumn("NEW_PART_NO");
-		// model.addColumn("RMA_NO");
-		// model.addColumn("REAL_LOCATION");
-
-		// CELL col=8 VALUE=STRING value=APPLICANT
-		// CELL col=32 VALUE=STRING value=IS_KEY_PART
-		// CELL col=43 VALUE=STRING value=LOCATION
-		// CELL col=47 VALUE=STRING value=NEW_PART_DESC
-		// CELL col=48 VALUE=STRING value=NEW_PART_NO
-		// CELL col=86 VALUE=STRING value=RMA_NO
-		// CELL col=89 VALUE=STRING value=REAL_LOCATION
-
-		allocatedItems = new ArrayList<AllocatedItem>();
-		allocatedItems = ReadAllocatedFromXls.read();
-
-		// 90AZ0080-R90031
-		// 60-N86MB1000-A02
-
+		setDefault();
 		AllocatedItem seachForAllocatedItem = new AllocatedItem();
 		seachForAllocatedItem.setNewPartNo(newPN.trim());
-
-		//System.out.println(model.getRowCount()); // set Model to null
-		//System.out.println(allocatedItems.contains(seachForAllocatedItem));
-
-	
 		if (allocatedItems.contains(seachForAllocatedItem)) {
-			if(selectedAllocatedItems != null)
-				selectedAllocatedItems.clear();
-			
 			for (int i = 0; i < allocatedItems.size(); i++) {
 				AllocatedItem allocatedItem = allocatedItems.get(i);
 				if (i == 0) { // Ueberschrift ableiten
