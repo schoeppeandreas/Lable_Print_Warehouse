927f13d9cd02c0ca453f6bffbb6d52fcaadf44e9
aschoeppe <aschoeppe@NB-DSDX024345.rts.rts-services.de>
2017-11-29 17:46:07 +0100

    save

--- src/frames/MainFrame.java
+++ src/frames/MainFrame.java
@@ -3,6 +3,8 @@ package frames;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JTable;
 import javax.swing.JScrollPane;
@@ -12,7 +14,6 @@ import java.util.List;
 import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.table.TableModel;
 
 public class MainFrame {
 
@@ -20,6 +21,7 @@ public class MainFrame {
 	private JTable table;
 	private JTextField textFieldNewPN;
 	private List<AllocatedItem> allocatedItems;
+	private List<AllocatedItem> selectedAllocatedItems = new ArrayList<>();
 
 	/**
 	 * Launch the application.
@@ -44,6 +46,24 @@ public class MainFrame {
 		initialize();
 	}
 
+	
+//	private static AbstractAction action() {
+//		
+//		AbstractAction a = new AbstractAction()
+//		{
+//			private static final long serialVersionUID = 1L;
+//			@Override
+//		    public void actionPerformed(ActionEvent e)
+//		    {
+//		        System.out.println("some action");
+//		    }
+//		};
+//		
+//		return a;
+//		
+//	}
+
+	
 	/**
 	 * Initialize the contents of the frame.
 	 */
@@ -53,11 +73,16 @@ public class MainFrame {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 
-		JButton btnButton = new JButton("Load");
+		
+
+		
+		
+		JButton btnButton = new JButton("Label drucken");
 		btnButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				DefaultTableModel model = new DefaultTableModel();
-
+				table.setModel(model);
+				
 				// model.addColumn("APPLICANT");
 				// model.addColumn("IS_KEY_PART");
 				// model.addColumn("LOCATION");
@@ -78,18 +103,22 @@ public class MainFrame {
 				allocatedItems = ReadAllocated.read();
 
 				// 90AZ0080-R90031
+				// 60-N86MB1000-A02
 
 				AllocatedItem seachForAllocatedItem = new AllocatedItem();
-				seachForAllocatedItem.setNewPartNo(textFieldNewPN.getText());
+				seachForAllocatedItem.setNewPartNo(textFieldNewPN.getText().trim());
 
 				//System.out.println(model.getRowCount()); // set Model to null
 				//System.out.println(allocatedItems.contains(seachForAllocatedItem));
 
+			
 				if (allocatedItems.contains(seachForAllocatedItem)) {
+					if(selectedAllocatedItems != null)
+						selectedAllocatedItems.clear();
 					
 					for (int i = 0; i < allocatedItems.size(); i++) {
 						AllocatedItem allocatedItem = allocatedItems.get(i);
-						if (i == 0) { // Überschrift ableiten
+						if (i == 0) { // Ueberschrift ableiten
 							Object[] obj = allocatedItem.getObjectArr();
 							for (int j = 0; j < obj.length; j++) {
 								model.addColumn(obj[j].toString());
@@ -97,17 +126,13 @@ public class MainFrame {
 						} else { // String Daten
 							if(allocatedItem.equals(seachForAllocatedItem)) {
 								model.addRow(allocatedItem.getObjectArr());	
+								selectedAllocatedItems.add(allocatedItem);
 							}
 						}
 					}
-
-					// for (AllocatedItem allocatedItem : allocatedItems) {
-					// model.addRow(allocatedItem.getObjectArr());
-					// }
+					Tsp400Label_Allocated.output(selectedAllocatedItems);
+					
 				}
-
-				table.setModel(model);
-
 				// JOptionPane.showMessageDialog(null, textField.getText());
 			}
 		});
@@ -126,5 +151,7 @@ public class MainFrame {
 		textFieldNewPN.setBounds(126, 6, 241, 26);
 		frame.getContentPane().add(textFieldNewPN);
 		textFieldNewPN.setColumns(10);
+		
 	}
+	
 }
---
+++ src/frames/Tsp400Label_Allocated.java
@@ -0,0 +1,101 @@
+package frames;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+public class Tsp400Label_Allocated {
+
+	private static String getTxtLine(String text) {
+		StringBuffer sb = new StringBuffer();
+		sb.append((char) 27);
+		sb.append(text);
+		sb.append((char) 10);
+		sb.append((char) 00);
+		return sb.toString();
+	}
+
+	public static void output(List<AllocatedItem> selectedAllocatedItems) {
+		StringBuffer sb = new StringBuffer();
+		for (AllocatedItem allocatedItem : selectedAllocatedItems) {
+			sb.append(getTxtLine("#1,0005"));
+			sb.append(getTxtLine("#2,0100"));
+			//sb.append(getTxtLine("R2"));
+			sb.append(getTxtLine("C"));
+			sb.append(getTxtLine("D0800"));
+			
+			//LableID;AbstandLinks;AbstandOben
+			
+			sb.append(getTxtLine("PC01;0005,0005,1,2,4,00,01")); //Applicant
+			sb.append(getTxtLine("PC02;0005,0090,1,2,2,00,02")); //RmaNo
+			sb.append(getTxtLine("PC03;0005,0160,1,2,2,00,02")); //Location
+			sb.append(getTxtLine("PC04;0005,0240,1,2,4,00,02")); //PN No
+			sb.append(getTxtLine("PC05;0005,0380,1,2,1,00,02")); //PN Desc
+			sb.append(getTxtLine("PC06;0560,0720,1,2,2,00,02")); //KeyPart
+			
+			sb.append(getTxtLine("PC07;0005,0440,1,2,4,00,02")); //Replaced
+			sb.append(getTxtLine("PC08;0005,0530,1,2,4,00,02")); //DOA
+			sb.append(getTxtLine("PC09;0005,0620,1,2,4,00,02")); //Gutteil
+			
+			sb.append(getTxtLine("PB00;0400,0090,1,7,0,0050")); //RmaNo Barcode
+			sb.append(getTxtLine("PB01;0005,0310,1,7,0,0060")); //PN No Barcode
+
+
+			sb.append(getTxtLine("B"));
+			
+			sb.append(getTxtLine("RC01;" + allocatedItem.getApplicant()));
+			sb.append(getTxtLine("RC02;RMA:" + allocatedItem.getRmaNo()));
+			sb.append(getTxtLine("RC03;Location: " + allocatedItem.getLocation() + "(" +  allocatedItem.getRealLocation() + ")" ));
+			sb.append(getTxtLine("RC04;" + allocatedItem.getNewPartNo()));
+			sb.append(getTxtLine("RC05;" + allocatedItem.getNewPartDesc()));
+			sb.append(getTxtLine("RC06;KeyPart:" + allocatedItem.getIsKeyPart())); //KeyPart
+			
+			sb.append(getTxtLine("RC07;( ) Replaced - BAD"));
+			sb.append(getTxtLine("RC08;( ) DOA - BAD"));
+			sb.append(getTxtLine("RC09;( ) Gutteil - Good"));
+			
+			
+			sb.append(getTxtLine("RB00;" + allocatedItem.getRmaNo()));
+			sb.append(getTxtLine("RB01;" + allocatedItem.getNewPartNo()));
+
+
+			sb.append(getTxtLine("I"));
+		}
+		createFile(sb, "label.dat");
+		printFile("cmd.exe /C Label.bat");
+	}
+
+	private static void printFile(String cmd) {
+		Runtime run = Runtime.getRuntime();
+		Process p = null;
+		try {
+			p = run.exec(cmd);
+			p.waitFor();
+			System.out.println(p.exitValue());
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			p.destroy();
+		}
+	}
+
+	private static void createFile(StringBuffer sb, String fileName) {
+		PrintWriter pWriter = null;
+		try {
+			pWriter = new PrintWriter(new BufferedWriter(new FileWriter(fileName)));
+			pWriter.print(sb);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			if (pWriter != null) {
+				pWriter.flush();
+				pWriter.close();
+			}
+		}
+	}
+}
